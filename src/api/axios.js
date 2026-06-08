5e55f93d2af6616ec49a87201cc61fdd35e28c5e
Pran2K22 <pranjulcg@gmail.com>
2026-06-09 01:26:12 +0530

    added the base url for the render

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,5 +3,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  //baseURL: "http://localhost:5000/api",
+  baseURL:"https://ai-interview-coach-server-3ato.onrender.com"
 });
