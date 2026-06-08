4393bfb608c3ed645a2eff7499292aea9895b745
Pran2K22 <pranjulcg@gmail.com>
2026-06-09 01:43:03 +0530

    added the base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,5 +4,5 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "http://localhost:5000/api",
-  baseURL:"https://ai-interview-coach-server-3ato.onrender.com"
+  baseURL:"https://ai-interview-coach-server-3ato.onrender.com/"
 });
